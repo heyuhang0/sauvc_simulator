9d23719d1b845828e4f5a475f659e42781d906eb
heyuhang0 <heyuhang0@outlook.com>
2020-01-07 15:51:18 +0800

    Add thruster manager and simple teleop

---
+++ bubbles_adapter/scripts/basic_teleop.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+
+class BasicTeleopNode:
+    def __init__(self):
+        rospy.init_node('basic_teleop')
+        rospy.Subscriber('/joy', Joy, self.callback)
+        self.pub = rospy.Publisher('/basic_teleop/output', Twist, queue_size=10)
+
+    def callback(self, msg: Joy):
+        output = Twist()
+        output.linear.x = msg.axes[1]
+        output.linear.y = 0
+        output.linear.z = msg.axes[2] - msg.axes[5]
+        output.angular.x = -msg.axes[3]
+        output.angular.y = msg.axes[4]
+        output.angular.z = -msg.axes[0]
+        self.pub.publish(output)
+
+
+if __name__ == '__main__':
+    BasicTeleopNode()
+    rospy.spin()
---
+++ bubbles_adapter/scripts/thruster_manager.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from typing import Sequence
+
+import rospy
+from geometry_msgs.msg import Twist
+from uuv_gazebo_ros_plugins_msgs.msg import FloatStamped
+
+
+class Thruster:
+    def __init__(self, node_name: str):
+        self.pub = rospy.Publisher(node_name + '/input', FloatStamped, queue_size=10)
+
+    def publish(self, value: float):
+        output = FloatStamped()
+        output.header.stamp = rospy.Time.now()
+        output.data = value
+        self.pub.publish(output)
+
+
+class ThrusterManagerNode:
+    def __init__(self):
+        self.node_name = 'thruster_manager'
+        rospy.init_node(self.node_name)
+
+        self.tranform_matrix = np.array([
+            # x  y   z roll pitch yaw
+            [0,  0,  1,  1, -1,  1],  # left front motor
+            [0,  0,  1, -1, -1,  1],  # right front motor
+            [1,  0,  0,  0,  0,  1],  # left rear motor
+            [1,  0,  0,  0,  0, -1],  # right rear motor
+            [0,  0,  1,  0,  1,  1]   # rear motor
+        ])
+        self.scale = 10
+        self.num_thrusters = len(self.tranform_matrix)
+        self.thrusters = [Thruster(f'thrusters/{i}') for i in range(self.num_thrusters)]
+
+        rospy.Subscriber(self.node_name + '/input', Twist, self.input_callback)
+
+        rospy.loginfo("ThrusterManagerNode started")
+
+    def input_callback(self, msg: Twist):
+        control_vector = np.array([
+            [msg.linear.x], [msg.linear.y], [msg.linear.z],
+            [msg.angular.x], [msg.angular.y], [msg.angular.z]
+        ])
+        output = self.scale * self.tranform_matrix @ control_vector
+        self.update_thrusters(output)
+
+    def update_thrusters(self, output: Sequence[float]):
+        for i in range(self.num_thrusters):
+            self.thrusters[i].publish(output[i])
+
+
+if __name__ == '__main__':
+    ThrusterManagerNode()
+    rospy.spin()
